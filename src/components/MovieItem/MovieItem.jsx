2f748bbe79f4a15e762d475e0040d449e7cd594a
Sagethh <tate.feige@gmail.com>
2021-07-24 15:09:56 -0500

    added modal to movie poster click

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -9,18 +9,24 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { useDispatch, useSelector } from 'react-redux';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-const useStyles = makeStyles({
-    root: {
-      maxWidth: 345,
-    },
-    media: {
-      height: 500,
-    },
-  });
+const useStyles = makeStyles({root: {width: 350,}, media: {height: 500,}});
+const useModal = makeStyles((theme) => ({
+    modal: {display: 'flex', alignItems: 'center', justifyContent: 'center', width: "50%", margin: "auto"},
+    paper: {backgroundColor: theme.palette.background.paper, border: '2px solid #000', boxShadow: theme.shadows[5], padding: theme.spacing(2, 4, 3),},
+}));
 
 function MovieItem(movie) {
     const classes = useStyles();
+    const modal = useModal();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {setOpen(true);};
+    const handleClose = () => {
+    setOpen(false);
+  };
 
 
     const test = () => {
@@ -30,34 +36,44 @@ function MovieItem(movie) {
 
 
     return (
-        <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            onClick={test}
-            className={classes.media}
-            src={movie.movie.poster}
-            title={movie.movie.title}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {movie.movie.title}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              {movie.movie.description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
-
+        <div>
+            <Modal
+                aria-labelledby="Movie-Modal"
+                aria-describedby={movie.movie.title}
+                className={modal.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={modal.paper}>
+                        <h2 id="transition-modal-title">{movie.movie.title}</h2>
+                        <p id="transition-modal-description">{movie.movie.description}</p>
+                    </div>
+                </Fade>
+            </Modal>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        onClick={test}
+                        className={classes.media}
+                        src={movie.movie.poster}
+                        title={movie.movie.title}
+                        onClick={handleOpen}
+                    />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {movie.movie.title}
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
     );
 };
 
