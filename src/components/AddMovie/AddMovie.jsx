e6e0da0eee12f6ca8011dab49dffeee31414d83b
Sagethh <tate.feige@gmail.com>
2021-07-24 19:58:45 -0500

    added a lot of multigenre select, still a lot to go

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -14,6 +14,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import CloseIcon from '@material-ui/icons/Close';
 import PublishIcon from '@material-ui/icons/Publish';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
 
 const useModal = makeStyles((theme) => ({
     modal: {display: 'flex', alignItems: 'center', justifyContent: 'center', width: "50%", margin: "auto"},
@@ -21,19 +23,34 @@ const useModal = makeStyles((theme) => ({
 }));
 
 function AddMovie() {
-     const dispatch = useDispatch();
+    const [genres, setGenres] = React.useState({
+        Adventure: false,
+        Animated: false,
+        Biographical: false,
+        Comedy: false,
+        Disaster: false,
+        Drama: false,
+        Epic: false,
+        Fantasy: false,
+        Musical: false,
+        Romantic: false,
+        Science_Fiction: false,
+        Space_Opera: false,
+        Superhero: false,
+      });
+    const modal = useModal();
+    const dispatch = useDispatch();
     const [movie, setMovie] = useState([]);
     const [movieTitle, setMovieTitle] = useState('');
     const [movieGenre, setMovieGenre] = useState('');
     const [movieDescription, setMovieDescription] = useState('');
     const [moviePoster, setMoviePoster] = useState('');
     const [open, setOpen] = React.useState(false);
-    const modal = useModal();
+    const [genreModalOpen, setGenreModalOpen] = React.useState(false);
+    const openGenreModal = () => {setGenreModalOpen(true);};
+    const closeGenreModal = () => {setGenreModalOpen(false);};
     const handleOpen = () => {setOpen(true);};
     const handleClose = () => {setOpen(false);};
-    const numberHandler = [{value: 0,label: "Select An Option"},{value: 1,label: "1"},{value: 2,label: "2"},{value: 3,label: "3"},{value: 4,label: "4"},{value: 5,label: "5"},{value: 6,label: "6"},{value: 7,label: "7"},{value: 8,label: "8"},{value: 9,label: "9"},{value: 10,label: "10"},];
-    // big long thingy for materialUI, gives options 1-10 for the dropdown
-
     const submit = () => {
         if (movieTitle == "" || movieDescription == "" || moviePoster == "") {
             console.log('nice try bitch, fill it in');
@@ -47,8 +64,44 @@ function AddMovie() {
         })
     }
 
+    const changer = (event) => {
+        setGenres({ ...genres, [event.target.name]: event.target.checked });
+        console.log(genres);
+    }
+    
+
     return (
         <>
+        <Modal
+                aria-labelledby="Select Movie Genres Modal"
+                aria-describedby="Select Movie Genres"
+                className={modal.modal}
+                open={genreModalOpen}
+                onClose={closeGenreModal}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+            <Fade in={genreModalOpen}>
+                <div className={modal.paper}>
+                <FormControlLabel checked={genres.Adventure} name="Adventure" control={<Switch color="primary" />} label="Adventure" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Animated" control={<Switch color="primary" />} label="Animated" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Biographical" control={<Switch color="primary" />} label="Biographical" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Comedy" control={<Switch color="primary" />} label="Comedy" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Disaster" control={<Switch color="primary" />} label="Disaster" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Drama" control={<Switch color="primary" />} label="Drama" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Epic" control={<Switch color="primary" />} label="Epic" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Fantasy" control={<Switch color="primary" />} label="Fantasy" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Musical" control={<Switch color="primary" />} label="Musical" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Romantic" control={<Switch color="primary" />} label="Romantic" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Science_Fiction" control={<Switch color="primary" />} label="Science Fiction" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Space-Opera" control={<Switch color="primary" />} label="Space-Opera" labelPlacement="start" onChange={changer}/>
+                <FormControlLabel checked={genres.Animated} name="Superhero" control={<Switch color="primary" />} label="Superhero" labelPlacement="start" onChange={changer}/>
+                </div>
+            </Fade>
+            </Modal>
             <Modal
                 aria-labelledby="Upload Movie Modal"
                 aria-describedby="Upload a movie"
@@ -69,10 +122,10 @@ function AddMovie() {
                 <TextField id="Movie-Description" label="Movie Description" variant="outlined" value={movieDescription} onChange={(event) => setMovieDescription(event.target.value)}/>
                 <br />
                 <br />
-                <TextField id="Movie-Genre" label="Movie Genre" variant="outlined" value={movieGenre} onChange={(event) => setMovieGenre(event.target.value)}/>
-                <br />
-                <br />
                 <TextField id="Movie-Poster-URL" label="Movie Poster URL" variant="outlined" value={moviePoster} onChange={(event) => setMoviePoster(event.target.value)}/>
+                <br />
+                <br />
+                <Button variant="contained" color="primary" endIcon={<PublishIcon />} onClick={openGenreModal}>Select Genres</Button>
                 <br />
                 <br />
                 <Button variant="contained" color="primary" startIcon={<CloseIcon />} onClick={handleClose}>Close</Button>
